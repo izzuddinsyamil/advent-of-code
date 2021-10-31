dbbb43d34fd525b736a6888cd8f10fc9c5900f88
M Syamil <muhammad.syamil@amartha.com>
2021-10-31 14:51:35 +0700

    finished 2020 day 3 part 2

--- 2020/day3/main.go
+++ 2020/day3/main.go
@@ -25,21 +25,37 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var r, c, treeCount int
-	for r < len(grid) {
-		for c > len(grid[r]) {
-			//expand the grid
-			expandRow(r, grid)
+	slopes := [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+	_ = slopes
+
+	res := 1
+	for _, slope := range slopes {
+		var r, c, treeCount int
+
+		for r < len(grid) {
+			for c >= len(grid[r]) {
+				//expand the grid
+				expandRow(r, grid)
+			}
+
+			// fmt.Println(r, c, len(grid[r]), grid[r])
+			if grid[r][c] == "#" {
+				treeCount += 1
+			}
+			c += slope[0]
+			r += slope[1]
 		}
 
-		if grid[r][c] == "#" {
-			treeCount += 1
-		}
-		c += 3
-		r += 1
+		res *= treeCount
 	}
 
-	fmt.Println(treeCount)
+	fmt.Println(res)
 }
 
 func expandRow(r int, grid [][]string) {
